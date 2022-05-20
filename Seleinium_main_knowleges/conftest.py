1a8918a9e882f0eb0d25e5ddb3354ab429e9b837
shnopiqa <vladzhoir231@gmail.com>
2022-05-20 19:39:58 +0300

    confest.py - не изменял base_page.py - пустая страница main_page.py - создал класс с конструкторм который принимает браузер и url, далее создал функцию которая открывает ссылку в браузере

--- Seleinium_main_knowleges/conftest.py
+++ Seleinium_main_knowleges/conftest.py
@@ -25,16 +25,19 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
 
-    user_language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
     browser = None
+    user_language = request.config.getoption("language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         # browser = webdriver.Firefox()
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_languages', user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
